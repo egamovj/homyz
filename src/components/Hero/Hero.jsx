ddfdf99865ef4d6d64ab8c9df5ef0bb21afcbade
egamovj <egamovj90@gmail.com>
2024-04-18 17:52:15 +0500

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import "./Hero.css";
 
 import HeroImg from "../../assets/hero-image.png";
 import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 
 const Hero = () => {
   return (
@@ -16,14 +17,42 @@ const Hero = () => {
           </div>
 
           <div className="flexColStart hero-desc">
-            <span>Find a variety properties that suit you very easily</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">
+              Find a variety properties that suit you very easily
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
           </div>
 
           <div className="flexCenter search-bar">
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
+          </div>
+
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Products</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={28} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Award Winning</span>
+            </div>
           </div>
         </div>
 
