ee5aac67897ba26551983170864495971aea9fa2
egamovj <egamovj90@gmail.com>
2024-04-18 18:08:27 +0500

    companies section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 
 import "./App.css";
+import Companies from "./components/Companies/Companies";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Header />
           <Hero />
         </div>
+        <Companies />
       </div>
     </>
   );
---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,21 @@
+import "./Companies.css";
+
+import Prologis from "../../assets/prologis.png";
+import Tower from "../../assets/tower.png";
+import Equinix from "../../assets/equinix.png";
+import Realty from "../../assets/realty.png";
+
+const Companies = () => {
+  return (
+    <section>
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src={Prologis} alt="" />
+        <img src={Tower} alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
